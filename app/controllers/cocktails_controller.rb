40523e2f48aeb237f9c6491665d9f23b413c3391
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-08 17:39:56 +0100

    improve css

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,9 +21,29 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :card_picture, :show_picture, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308114542) do
+ActiveRecord::Schema.define(version: 20180308161504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180308114542) do
     t.datetime "updated_at", null: false
     t.string "card_picture"
     t.string "show_picture"
+    t.text "description"
   end
 
   create_table "doses", force: :cascade do |t|
