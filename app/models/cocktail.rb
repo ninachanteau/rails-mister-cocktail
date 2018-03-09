e7f4f8b2162a20383c1bc434c6e177e1a3c63100
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-09 15:49:03 +0100

    validates pictures

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,9 @@ class Cocktail < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  validates :card_picture, presence: true
+  validates :show_picture, presence: true
+  validates :name, presence: true, uniqueness: true
   mount_uploader :card_picture, PhotoUploader
   mount_uploader :show_picture, PhotoUploader
 end
