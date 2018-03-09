9ee1516178e17299d4b2cf5f927fa8730530247a
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-09 15:01:58 +0100

    create the cocktail seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,39 @@ Ingredient.destroy_all
 
 puts 'Creating cocktails...'
 
+cocktail = Cocktail.new(name: "Bloody Mary")
+cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/BloodyMary-Card.jpeg")
+cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/BloodyMary-Show.jpeg")
+cocktail.save
+
+cocktail = Cocktail.new(name: "Champagne")
+cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/Champagne-Card.jpeg")
+cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/Champagne-Show.jpeg")
+cocktail.save
+
+cocktail = Cocktail.new(name: "Gin Tonic")
+cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/GinTonic-Card.jpeg")
+cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/GinTonic-Show.jpeg")
+cocktail.save
+
+cocktail = Cocktail.new(name: "Margarita")
+cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/Margarita-Card.jpeg")
+cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/Margarita-Show.jpeg")
+cocktail.save
+
+cocktail = Cocktail.new(name: "Mojito")
+cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/Mojito-Card.jpeg")
+cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/Mojito-Show.jpeg")
+cocktail.save
+
+cocktail = Cocktail.new(name: "Old Fashioned")
+cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/OldFashioned-Card.jpeg")
+cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/OldFashioned-Show.jpeg")
+cocktail.save
+
 cocktail = Cocktail.new(name: "Piña Colada")
-cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/PinaColada-Card.png")
-cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/PinaColada-Show.png")
+cocktail.remote_card_picture_url = File.join(Rails.root, "db/photos/PinaColada-Card.jpeg")
+cocktail.remote_show_picture_url = File.join(Rails.root, "db/photos/PinaColada-Show.jpeg")
 cocktail.save
 
 puts 'Creating ingredients...'
