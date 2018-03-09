45de9dc8d6d95a50cb5e2952e310eac50495bccd
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-09 16:34:18 +0100

    add the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
   resources :cocktails do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [ :new, :create ]
